f7b6197449308f3fdcba000580f26304b16476d3
Fung Wing Kit <wengee@gmail.com>
2020-05-28 14:13:20 +0800

    commit

---
+++ src/Application.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+/**
+ * @author   Fung Wing Kit <wengee@gmail.com>
+ * @version  2020-05-28 14:11:59 +0800
+ */
+
+namespace fwkit\Console;
+
+use Symfony\Component\Console\Application as SymfonyApplication;
+
+class Application extends SymfonyApplication
+{
+}
