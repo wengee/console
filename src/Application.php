77aad156d58f6e9b158d5a9e06052211bfab004a
Fung Wing Kit <wengee@gmail.com>
2020-06-02 11:51:26 +0800

    commit

--- src/Application.php
+++ src/Application.php
@@ -1,12 +1,14 @@
 <?php declare(strict_types=1);
 /**
  * @author   Fung Wing Kit <wengee@gmail.com>
- * @version  2020-06-02 11:50:22 +0800
+ * @version  2020-06-02 11:51:23 +0800
  */
 
 namespace fwkit\Console;
 
 use Symfony\Component\Console\Application as SymfonyApplication;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 class Application extends SymfonyApplication
 {
